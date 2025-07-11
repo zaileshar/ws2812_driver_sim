535298b4f5c1c530a36f3d57d08f1c7209060ade
zaileshar <zaileshar@gmail.com>
2025-07-11 10:36:49 +0530

    refactor api routing

---
+++ pwm_core.v
@@ -0,0 +1,10 @@
+module pwm_core (
+    input clk,
+    input rst_n,
+    output reg out
+);
+    always @(posedge clk) begin
+        if (!rst_n) out <= 0;
+        else out <= ~out;
+    end
+endmodule
