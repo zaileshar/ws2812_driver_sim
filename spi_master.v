6bf587c849b0ae0b11c978a724b0085ac24a8f57
zaileshar <zaileshar@gmail.com>
2025-07-12 18:52:28 +0530

    update dockerfile

---
+++ spi_master.v
@@ -0,0 +1,10 @@
+module spi_master (
+    input clk,
+    input rst_n,
+    output reg out
+);
+    always @(posedge clk) begin
+        if (!rst_n) out <= 0;
+        else out <= ~out;
+    end
+endmodule
